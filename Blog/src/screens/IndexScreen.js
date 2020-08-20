47605b2dc8db3afcacebca58463d586a7ca51831
rohan2734 <jagandevaki1@gmail.com>
2020-08-20 09:34:50 +0530

    added useContext login in IndexScreen

--- Blog/src/screens/IndexScreen.js
+++ Blog/src/screens/IndexScreen.js
@@ -1,23 +1,24 @@
 import React,{useContext} from 'react';
 import {View,Text,StyleSheet,FlatList,Button} from 'react-native';
-import {Context } from '../context/BlogContext';
+import {Context} from '../context/BlogContext';
 
+import { AntDesign } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const {state,addBlogPost} = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList 
         data={state}
         keyExtractor={blogPost => blogPost.title }
         renderItem={({item}) => {
           return (
-            <Text>
-              {item.title}
-            </Text>
+            <View style={styles.row}>
+              <Text style={styles.title} >{item.title}</Text>
+              <AntDesign name="delete" size={24} color="black" />
+            </View>
           )
         }}
       />
@@ -25,6 +26,18 @@ const IndexScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    paddingVertical:20,
+    border: 1,
+    borderColor:'gray',
+    paddingHorizontal:10
+  },
+  title: {
+    fontSize:18
+  }
+});
 
 export default IndexScreen;
