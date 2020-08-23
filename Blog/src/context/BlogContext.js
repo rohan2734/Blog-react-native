2d42329e24bd75989aace409aff7ac0d300079c6
rohan2734 <jagandevaki1@gmail.com>
2020-08-23 13:47:53 +0530

    added callback for addBlogPost

--- Blog/src/context/BlogContext.js
+++ Blog/src/context/BlogContext.js
@@ -8,10 +8,12 @@ const blogReducer = (state,action) => {
     case 'delete_blogPost':
       return state.filter((blogPost) =>blogPost.id !== action.payload);
     case 'add_blogPost':
-      return [...state,
+      return [
+        ...state,
         {
           id: Math.floor(Math.random()*99999),
-          title: `Blog Post #${state.length + 1}`
+          title: action.payload.title,
+          content:action.payload.content
         }
       ];
     default:
@@ -20,8 +22,9 @@ const blogReducer = (state,action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({type:'add_blogPost' });
+  return (title,content,callback) => {
+    dispatch({type:'add_blogPost',payload:{title,content:content} });
+    callback();
   }
 };
 
